8b3b8f57d74ea2f06bf9f1a3aeeb86207015376e
albertalvin8080 <albertalvin99@gmail.com>
2024-09-28 19:11:54 -0300

    feat(): square_plot_max_side()

---
+++ grooking_algorithms/chapter_4_square_plots.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int square_plot_max_side(double bigger_side, int smaller_side)
+{
+    if (smaller_side == 0)
+        return bigger_side;
+    else
+    {
+        int i = bigger_side / smaller_side;
+        int temp = bigger_side;
+        bigger_side = smaller_side;
+        smaller_side = temp - smaller_side * i;
+        return square_plot_max_side(bigger_side, smaller_side);
+    }
+}
+
+// gcc -o main chapter_4_square_plots.c && ./main
+int main()
+{
+    /*
+    Unexpected good behavior: if you switch the positions of the bigger and smaller sides,
+    this algorithm will first invert their position.
+    */
+    int bigger_side = 1680; // try 80
+    int smaller_side = 640; // try 90
+    int plot_side = square_plot_max_side(bigger_side, smaller_side);
+    printf("Plot side = %d\n", plot_side);
+    return 0;
+}
